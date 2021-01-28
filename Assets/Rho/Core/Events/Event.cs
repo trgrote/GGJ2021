28d463d18279230f1eea0a8ad7298ee5fc8113ed
Taylor Grote <grotese@gmail.com>
2021-01-28 17:20:27 -0600

    Add RhoUtils

---
+++ Assets/Rho/Core/Events/Event.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace rho
+{
+    [CreateAssetMenu(menuName = "Rho/Event")]
+    public class Event : ScriptableObject
+    {
+        List<EventListener> _listeners = new List<EventListener>();
+
+        public void Raise()
+        {
+            for (var i = _listeners.Count - 1; i >= 0 ; --i)
+            {
+                _listeners[i].OnEventRaised();
+            }
+        }
+
+        public void Register(EventListener listener)
+        {
+            _listeners.Add(listener);
+        }
+
+        public void Unregister(EventListener listener)
+        {
+            _listeners.Remove(listener);
+        }
+    }
+
+    public class Event<TArg> : ScriptableObject
+    {
+        List<EventListener<TArg>> _listeners = new List<EventListener<TArg>>();
+
+        public void Raise(TArg arg)
+        {
+            for (var i = _listeners.Count - 1; i >= 0 ; --i)
+            {
+                _listeners[i].OnEventRaised(arg);
+            }
+        }
+
+        public void Register(EventListener<TArg> listener)
+        {
+            _listeners.Add(listener);
+        }
+
+        public void Unregister(EventListener<TArg> listener)
+        {
+            _listeners.Remove(listener);
+        }
+    }
+
+    public class Event<TArg1, TArg2> : ScriptableObject
+    {
+        List<EventListener<TArg1, TArg2>> _listeners = new List<EventListener<TArg1, TArg2>>();
+
+        public void Raise(TArg1 arg11, TArg2 arg2)
+        {
+            for (var i = _listeners.Count - 1; i >= 0 ; --i)
+            {
+                _listeners[i].OnEventRaised(arg11, arg2);
+            }
+        }
+
+        public void Register(EventListener<TArg1, TArg2> listener)
+        {
+            _listeners.Add(listener);
+        }
+
+        public void Unregister(EventListener<TArg1, TArg2> listener)
+        {
+            _listeners.Remove(listener);
+        }
+    }
+
+    public class Event<TArg1, TArg2, TArg3> : ScriptableObject
+    {
+        List<EventListener<TArg1, TArg2, TArg3>> _listeners = new List<EventListener<TArg1, TArg2, TArg3>>();
+
+        public void Raise(TArg1 arg11, TArg2 arg2, TArg3 arg3)
+        {
+            for (var i = _listeners.Count - 1; i >= 0 ; --i)
+            {
+                _listeners[i].OnEventRaised(arg11, arg2, arg3);
+            }
+        }
+
+        public void Register(EventListener<TArg1, TArg2, TArg3> listener)
+        {
+            _listeners.Add(listener);
+        }
+
+        public void Unregister(EventListener<TArg1, TArg2, TArg3> listener)
+        {
+            _listeners.Remove(listener);
+        }
+    }
+}
---
+++ Assets/Rho/Core/Events/EventListener.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace rho
+{
+    public class EventListener : MonoBehaviour
+    {
+        public Event Event;
+        public UnityEvent Response;
+
+        void OnEnable()
+        {
+            Event.Register(this);
+        }
+
+        void OnDisable()
+        {
+            Event.Unregister(this);
+        }
+
+        public void OnEventRaised()
+        {
+            Response.Invoke();
+        }
+    }
+
+    public class EventListener<TArg> : MonoBehaviour
+    {
+        public Event<TArg> Event;
+        public UnityEvent<TArg> Response;
+
+        void OnEnable()
+        {
+            Event.Register(this);
+        }
+
+        void OnDisable()
+        {
+            Event.Unregister(this);
+        }
+
+        public void OnEventRaised(TArg arg)
+        {
+            Response.Invoke(arg);
+        }
+    }
+
+    public class EventListener<TArg1, TArg2> : MonoBehaviour
+    {
+        public Event<TArg1, TArg2> Event;
+        public UnityEvent<TArg1, TArg2> Response;
+
+        void OnEnable()
+        {
+            Event.Register(this);
+        }
+
+        void OnDisable()
+        {
+            Event.Unregister(this);
+        }
+
+        public void OnEventRaised(TArg1 arg1, TArg2 arg2)
+        {
+            Response.Invoke(arg1, arg2);
+        }
+    }
+
+    public class EventListener<TArg1, TArg2, TArg3> : MonoBehaviour
+    {
+        public Event<TArg1, TArg2, TArg3> Event;
+        public UnityEvent<TArg1, TArg2, TArg3> Response;
+
+        void OnEnable()
+        {
+            Event.Register(this);
+        }
+
+        void OnDisable()
+        {
+            Event.Unregister(this);
+        }
+
+        public void OnEventRaised(TArg1 arg1, TArg2 arg2, TArg3 arg3)
+        {
+            Response.Invoke(arg1, arg2, arg3);
+        }
+    }
+}
