c7ca867da4236c898ae04471f8525bbe808d3af9
Taylor Grote <grotese@gmail.com>
2021-01-31 16:09:16 -0600

    Add thud sound for some items

--- Assets/BabyGrabber.cs
+++ Assets/BabyGrabber.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class BabyGrabber : MonoBehaviour
 {
     [SerializeField] rho.RuntimeGameObjectSet _grabbles;
+    [SerializeField] rho.RuntimeGameObjectSet _weapons;
     [SerializeField] rho.Event _eatEvent;
+    [SerializeField] rho.Event _thudEvent;
 
     // Update is called once per frame
     public void OnBabyCollision(Collider2D collider)
@@ -14,6 +16,14 @@ public class BabyGrabber : MonoBehaviour
         {
             Destroy(collider.gameObject);
             _eatEvent.Raise();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (_weapons.Contains(collision.gameObject))
+        {
+            _thudEvent.Raise();
         }
     }
 }
