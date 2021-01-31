2a88ae9c802d50ea1c3924a0aa4be8916a62f2d2
Taylor Grote <grotese@gmail.com>
2021-01-31 09:33:06 -0600

    Add Falling_IntroDone event

--- Assets/Scripts/MusicHandler.cs
+++ Assets/Scripts/MusicHandler.cs
@@ -6,11 +6,19 @@ public class MusicHandler : MonoBehaviour
 {
     [SerializeField] AudioSource _introSource;
     [SerializeField] AudioSource _loopedSource;
+    [SerializeField] rho.Event _introOverEvent;
 
     // Start is called before the first frame update
     public void StartPlaying()
     {
         _introSource.Play();
         _loopedSource.PlayDelayed(_introSource.clip.length);
+        StartCoroutine(RaiseEventAfterDelay(_introSource.clip.length));
+    }
+
+    IEnumerator RaiseEventAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        _introOverEvent.Raise();
     }
 }
