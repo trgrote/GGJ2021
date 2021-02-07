fc161d00f53c0e27ae0d7226198e95717af0acef
Taylor Grote <grotese@gmail.com>
2021-02-07 12:53:39 -0600

    Add velocity clamping to anchor movement

--- Assets/Scripts/AnchorMovement.cs
+++ Assets/Scripts/AnchorMovement.cs
@@ -3,11 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class AnchorMovement : MonoBehaviour
 {
+    [SerializeField] Vector2 _minValues, _maxValues;
+    Rigidbody2D _rigidBody2d;
+
     public void OnMove(InputAction.CallbackContext context)
     {
         var value = context.ReadValue<Vector2>();
-        GetComponent<Rigidbody2D>().velocity = value * 5;
+        var newVelocity = value * 5;
+        _rigidBody2d.velocity = newVelocity;
+    }
+
+    void Start()
+    {
+        _rigidBody2d = GetComponent<Rigidbody2D>();
+        Debug.Log(_minValues);
+    }
+
+    void FixedUpdate()
+    {
+        var position = transform.localPosition;
+        var newVelocity = _rigidBody2d.velocity;
+        var changeRequried = false;
+
+        // Check to make sure we are in bounds, and it not, don't let change go FURTHER out of bounds
+        if (position.x <= _minValues.x)
+        {
+            newVelocity.x = Mathf.Clamp(newVelocity.x, 0, Mathf.Infinity);
+            changeRequried = true;
+        }
+        else if(position.x >= _maxValues.x)
+        {
+            newVelocity.x = Mathf.Clamp(newVelocity.x, Mathf.NegativeInfinity, 0);
+            changeRequried = true;
+        }
+
+        if (position.y <= _minValues.y)
+        {
+            newVelocity.y = Mathf.Clamp(newVelocity.y, 0, Mathf.Infinity);
+            changeRequried = true;
+        }
+        else if(position.y >= _maxValues.y)
+        {
+            newVelocity.y = Mathf.Clamp(newVelocity.y, Mathf.NegativeInfinity, 0);
+            changeRequried = true;
+        }
+
+        if (changeRequried)
+        {
+            _rigidBody2d.velocity = newVelocity;
+        }
     }
 }
