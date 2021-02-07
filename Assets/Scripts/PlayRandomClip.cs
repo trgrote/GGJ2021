757ffe7a1022757a84254464ed787ac9667d5795
Taylor Grote <grotese@gmail.com>
2021-02-07 11:54:47 -0600

    Add random baby laugh on eat food

---
+++ Assets/Scripts/PlayRandomClip.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRandomClip : MonoBehaviour
+{
+    [SerializeField] AudioSource _audioSource;
+    [SerializeField] AudioClip[] _clips;
+
+    public void Play()
+    {
+        _audioSource.Stop();
+        _audioSource.clip = _clips[Random.Range(0, _clips.Length)];
+        _audioSource.Play();
+    }
+}
